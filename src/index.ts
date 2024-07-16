9e4f7efa7f9497a8dfd95a98bc3f862392dc17f1
MengLinMaker <menglinmaker@gmail.com>
2024-07-17 04:33:01 +1000

    Chore: Add placeholder files from browser-compat

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
-export * from './foo'
-export * from './bar'
+/**
+ * Step 1) Entry point of plugin. Exports itself for eslint to use
+ * @author Meng Lin
+ */
+
+import type { Linter } from 'eslint'
+import pkg from '../package.json'
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+import recommended from './config/recommended'
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+// import all rules in lib/rules
+import compat from './rules/compat'
+
+const plugin = {
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+  },
+  configs: {},
+  rules: {
+    compat,
+  },
+}
+
+const configs = {
+  'flat/recommended': {
+    plugins: { compat: plugin },
+    ...recommended.flat,
+  } as Linter.FlatConfig,
+  recommended: {
+    plugins: ['compat'],
+    ...recommended.legacy,
+  } as Linter.Config,
+}
+plugin.configs = configs
+
+export = {
+  ...(plugin as typeof plugin & { configs: typeof configs }),
+  config: configs,
+}

d0a4269b453c4dd063bd7eb4b724326ed31015e8
MengLinMaker <menglinmaker@gmail.com>
2024-07-17 00:03:29 +1000

    Test: Add objectKey Type test

---
+++ tests/utils/objectKeys.unit.test.ts
@@ -0,0 +1,18 @@
+import { objectKeys } from './../../src/utils/objectKeys';
+
+import { describe, expect, expectTypeOf, it } from 'vitest'
+
+describe('objectKey', () => {
+  it('Return keys of object with correct types', () => {
+    const keys = objectKeys({
+      a: null,
+      'b ': 'b ',
+      c: () => { },
+      d: { d: null },
+    })
+    const expectedKeys = ['a', 'b ', 'c', 'd']
+    type expectedKeyType = "a" | "b " | "c" | "d"
+    expect(keys).toEqual(expectedKeys)
+    expectTypeOf(keys).toMatchTypeOf<expectedKeyType[]>()
+  })
+})

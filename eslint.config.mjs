32d286863f27a8be1e50cb7bd56a49a8fa7a2f5d
MengLinMaker <menglinmaker@gmail.com>
2024-07-17 00:05:37 +1000

    Chore: Remove unecessary browser-compat linting as this is a dev tool

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import pluginJs from '@eslint/js'
-import compat from 'eslint-plugin-compat'
 import tseslint from 'typescript-eslint'
 
 export default [
@@ -8,5 +7,4 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.strict,
-  compat.configs['flat/recommended'],
 ]

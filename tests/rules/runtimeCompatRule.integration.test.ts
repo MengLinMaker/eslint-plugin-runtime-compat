688813cdc9478246a18fac488abe823ec38c6d09
MengLinMaker <menglinmaker@gmail.com>
2024-11-04 21:16:21 +1100

    feat: detect class instantiation with NewExpression and type inference

--- tests/rules/runtimeCompatRule.integration.test.ts
+++ tests/rules/runtimeCompatRule.integration.test.ts
@@ -3,7 +3,7 @@ import { runtimeCompatRule } from '../../src/rules/runtime-compat'
 import { ruleTester } from './setup'
 
 const filterRuntimes: RuntimeName[] = ['node']
-const cacheErrorMsg =
+const cacheInstantiationError =
   "'Cache' - Unsupported API in node.\nDocs - https://developer.mozilla.org/docs/Web/API/Cache"
 
 ruleTester.run(
@@ -13,37 +13,24 @@ ruleTester.run(
     experimental: false,
   }),
   {
-    valid: [
-      {
-        code: 'fetch("https://www.google.com")',
-      },
-    ],
+    valid: ['fetch("https://www.google.com")'],
     invalid: [
       {
         // Detect unsupported API constructor call.
         code: /*javascript*/ `
-            const a = new Cache()
-            let b = new Cache()
-            b = new Cache()
+            const _Cache = Cache
+            let cache = new _Cache()
+            cache.add("test.html")
           `,
-        errors: [
-          { message: cacheErrorMsg },
-          { message: cacheErrorMsg },
-          { message: cacheErrorMsg },
-        ],
+        errors: [{ message: cacheInstantiationError }],
       },
       {
         // Detect unsupported API variable assignment.
         code: /*javascript*/ `
-            const n = Cache
-            let b = new Cache
-            b = new Cache
+            const cache = new Cache()
+            cache.add("test.html")
           `,
-        errors: [
-          { message: cacheErrorMsg },
-          { message: cacheErrorMsg },
-          { message: cacheErrorMsg },
-        ],
+        errors: [{ message: cacheInstantiationError }],
       },
     ],
   },

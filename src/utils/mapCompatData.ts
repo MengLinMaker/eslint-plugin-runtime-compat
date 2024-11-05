72591ea242a967bfccfb84ab9237c6e37957d276
MengLinMaker <menglinmaker@gmail.com>
2024-11-06 01:32:41 +1100

    chore: reduce type statements

--- src/utils/mapCompatData.ts
+++ src/utils/mapCompatData.ts
@@ -1,29 +1,31 @@
-import type { CompatStatement } from 'runtime-compat-data'
+import type { CompatData, CompatStatement } from 'runtime-compat-data'
 import { objectKeys } from './objectKeys'
 
 // Catch compile errors with constant.
 const __compat = '__compat' as never
+
+type ParsedCompatData = {
+  [key: string]: CompatStatement
+}
 
 /**
  * DFS map raw compat-data nodes with '__compat' key to a flat object.
  * @param compatData - Raw compat-data object.
  * @returns Flat object containing '__compat' data from raw compat-data.
  */
-export const mapCompatData = (compatData: object) => {
-  const parsedCompatData: {
-    [key: string]: CompatStatement
-  } = {}
+export const mapCompatData = (compatData: CompatData) => {
+  const parsedCompatData: ParsedCompatData = {}
 
-  const dfsParse = (compatData: object, parentKeys: never[] = []) => {
-    const keys = objectKeys(compatData) as never[]
+  const dfsParse = (compatData: CompatStatement | CompatData, parentKeys: string[] = []) => {
+    const keys = objectKeys(compatData)
     for (const key of keys) {
-      const subData = compatData[key]
+      const subData = compatData[key] as CompatStatement
       if (key === __compat) {
         // Prefer <Class> <Class> data for constructor compat for accuracy
         if (parentKeys.length > 1 && parentKeys[0] === parentKeys[1]) {
           // Reduce to single <Class> key to override previous compat data
-          const shortendedKey = parentKeys.slice(1, parentKeys.length)
-          parsedCompatData[JSON.stringify(shortendedKey)] = subData
+          const shortendedKey = JSON.stringify(parentKeys.slice(1, parentKeys.length))
+          parsedCompatData[shortendedKey] = subData
         } else {
           parsedCompatData[JSON.stringify(parentKeys)] = subData
         }

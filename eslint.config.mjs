6b2a890dfe7a9b6ee36735beca581548a68c1c16
MengLinMaker <menglinmaker@gmail.com>
2024-11-07 18:05:37 +1100

    chore: simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import runtimeCompat from './dist/index.js'
 
 export default [
   {
@@ -8,12 +7,4 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.strict,
-
-  // Custom config as example for local dev runtimes.
-  runtimeCompat.configs.custom(['node', 'bun', 'deno'], {
-    deprecated: true,
-    experimental: true,
-  }),
-  // runtimeCompat.configs.recommended
-  // runtimeCompat.configs.strict
 ]

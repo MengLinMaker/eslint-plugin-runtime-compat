1cd11b315ecfc8926837735af3be0d9eaea31025
MengLinMaker <menglinmaker@gmail.com>
2024-11-25 13:03:49 +1100

    chore: remove unecessary linting packages

--- e2e/import-test/eslint.config.js
+++ e2e/import-test/eslint.config.js
@@ -1,12 +1,7 @@
-import pluginJs from '@eslint/js'
 import runtimeCompat from '@menglinmaker/eslint-plugin-runtime-compat'
-import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
-  { files: ['**/*.ts'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   runtimeCompat.configs.recommended,
   {

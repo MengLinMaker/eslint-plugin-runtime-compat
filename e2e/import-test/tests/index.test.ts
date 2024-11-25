cb1dc8bd9694e3175eaabb810e2655c8d808ea3b
MengLinMaker <menglinmaker@gmail.com>
2024-11-25 12:44:21 +1100

    chore: successful multifile end to end test

---
+++ e2e/import-test/tests/index.test.ts
@@ -0,0 +1,9 @@
+import { spawnSync } from 'node:child_process'
+import { describe, expect, it } from 'vitest'
+
+describe('Multi file import linting test', () => {
+  it('should detect [Cache] incompatability', () => {
+    const { stdout } = spawnSync('pnpm', ['test:lint'])
+    expect(stdout.toString()).toContain('class - [Cache] - Unsupported standard API')
+  })
+})
